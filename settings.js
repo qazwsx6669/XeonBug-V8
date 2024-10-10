eca5f2626074c9dc547b2f47211e709d82221c8c
qazwsx6669 <120317923+qazwsx6669@users.noreply.github.com>
2024-10-11 00:10:18 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
-global.socialm = "GitHub: GlobalTechInfo"
+global.ownernomer = "201224078537"
+global.ownername = "Madara"
+global.ytname = "wa: R.Y.D"
+global.socialm = "kingdom: R.Y.D"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '201224078537'  //creator number
+global.ownername = 'Madara ' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.packname = 'Sticker By obito bot'
+global.author = 'GlobalTech\n\nContact: +201224078537'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VaDu0x6A89MhmCvo3H0X'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
